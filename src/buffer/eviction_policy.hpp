ab542a08231fc4280cfd75798b5704f212ca146d
Andrew Shilman <Andrew.Shilman@gmail.com>
2026-04-08 15:14:41 -0400

    EvictionPolicy layout class

---
+++ src/buffer/eviction_policy.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "common/config.hpp"
+#include <optional>
+#include <cstddef>
+
+namespace shilmandb {
+
+
+class EvictionPolicy {
+
+public:
+    virtual ~EvictionPolicy() = default;
+
+    //record that frame_id was accessed (move to MRU position in LRU)
+    virtual void RecordAccess(frame_id_t frame_id) = 0;
+
+    //mark frame as evicitable or not
+    virtual void SetEvictable(frame_id_t frame_id, bool evictable) = 0;
+
+    //select and return chosen frame
+    virtual std::optional<frame_id_t> Evict() = 0;
+
+    //remove frame from tracking
+    virtual void Remove(frame_id_t frame_id) = 0;
+
+    //num of evictable frames tracked
+    virtual size_t Size() const = 0;
+
+};
+
+}
