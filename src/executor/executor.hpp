3283a23f78fd8b46e98954fbcd7d703cf4ec87eb
Andrew Shilman <Andrew.Shilman@gmail.com>
2026-04-19 21:54:48 -0400

    feat: add ExecutionMode enum

--- src/executor/executor.hpp
+++ src/executor/executor.hpp
@@ -1,13 +1,18 @@
 #pragma once
-#include "planner/plan_node.hpp"
-#include "executor/executor_context.hpp"
-#include "types/tuple.hpp"
 #include "catalog/schema.hpp"
+#include "executor/data_chunk.hpp"
+#include "executor/executor_context.hpp"
+#include "planner/plan_node.hpp"
+#include "types/tuple.hpp"
 
 namespace shilmandb {
 
-class Executor {
+enum class ExecutionMode { 
+    TUPLE, 
+    VECTORIZED 
+};
 
+class Executor {
 public:
     Executor(const PlanNode* plan, ExecutorContext* ctx) : plan_(plan), ctx_(ctx) {}
     virtual ~Executor() = default;
@@ -16,11 +21,21 @@ public:
     virtual bool Next(Tuple* tuple) = 0;
     virtual void Close() = 0;
 
-    const Schema& GetOutputSchema() const {return plan_->output_schema;}
+    virtual bool NextBatch(DataChunk* chunk) {
+        chunk->Reset();
+        Tuple tuple;
+        while (!chunk->IsFull()) {
+            if (!Next(&tuple)) break;
+            chunk->AppendTuple(tuple);
+        }
+        return chunk->size() > 0;
+    }
+
+    const Schema& GetOutputSchema() const { return plan_->output_schema; }
 
 protected:
     const PlanNode* plan_;
     ExecutorContext* ctx_;
-
 };
-}
+
+}  // namespace shilmandb
--- src/executor/executor_factory.hpp
+++ src/executor/executor_factory.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include "executor/executor.hpp"
-#include "planner/plan_node.hpp"
 #include "executor/executor_context.hpp"
+#include "planner/plan_node.hpp"
 #include <memory>
 
 namespace shilmandb {
 
 class ExecutorFactory {
 public:
-    [[nodiscard]] static std::unique_ptr<Executor> CreateExecutor(const PlanNode* plan, ExecutorContext* ctx);
+    [[nodiscard]] static std::unique_ptr<Executor> CreateExecutor(const PlanNode* plan, ExecutorContext* ctx, ExecutionMode mode = ExecutionMode::TUPLE);
 };
 
 }  // namespace shilmandb
